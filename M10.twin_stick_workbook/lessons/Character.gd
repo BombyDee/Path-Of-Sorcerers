a27fb41b9119bbb4a74cf10a3ae0cb631eeddbdb
LeoVDang <dangleo730@gmail.com>
2025-01-15 11:28:05 -0800

    M10 L4 Character Movement

---
+++ M10.twin_stick_workbook/lessons/Character.gd
@@ -0,0 +1,34 @@
+extends Sprite2D
+
+
+const GODOT_BOTTOM = preload("res://player/godot_bottom.png")
+const GODOT_BOTTOM_RIGHT = preload("res://player/godot_bottom_right.png")
+const GODOT_RIGHT = preload("res://player/godot_right.png")
+const GODOT_UP = preload("res://player/godot_up.png")
+const GODOT_UP_RIGHT = preload("res://player/godot_up_right.png")
+
+const BOTTOM_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const BOTTOM_LEFT = Vector2.DOWN + Vector2.LEFT
+const UP_LEFT = Vector2.UP + Vector2.LEFT
+const UP_RIGHT = Vector2.UP + Vector2.RIGHT
+
+
+
+func _process(delta: float) -> void:
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
+	var direction_discrete := direction.sign()
+	match direction_discrete:
+		Vector2.RIGHT, Vector2.LEFT:
+			texture = GODOT_RIGHT
+		Vector2.UP:
+			texture = GODOT_UP
+		Vector2.DOWN:
+			texture = GODOT_BOTTOM
+		UP_RIGHT, BOTTOM_LEFT:
+			texture = GODOT_UP_RIGHT
+		BOTTOM_RIGHT, BOTTOM_LEFT:
+			texture = GODOT_BOTTOM_RIGHT
+	if direction_discrete.length() > 0:
+			flip_h = direction_discrete.x < 0.0
+			
+	
---
+++ M10.twin_stick_workbook/lessons/player.gd
@@ -0,0 +1,13 @@
+class_name Player extends CharacterBody2D
+
+@export var speed = 600.0 
+
+@export var drag_factor := 10.0
+
+func _physics_process(delta: float) -> void:
+	var move_direction : Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
+	var desired_velocity : Vector2 = speed * move_direction
+	var steering := desired_velocity - velocity
+	velocity += steering * drag_factor * delta
+	move_and_slide()
+	
